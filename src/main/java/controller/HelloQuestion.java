65f280149fd87498600828fc184aaa8063c21d47
madjid <madjidyous@gmail.com>
2017-12-07 10:45:53 +0100

    correction de correction de Hello

--- src/main/java/controller/HelloQuestion.java
+++ src/main/java/controller/HelloQuestion.java
@@ -1,7 +1,6 @@
 package controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import java.io.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,22 +15,19 @@ import model.entity.question.Question;
 @WebServlet("/hello")
 public class HelloQuestion extends HttpServlet {
 	
-       
-
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	return "hello question";
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+			
+response.setContentType("text/html");
+  PrintWriter out = response.getWriter();
+  out.append("Your string goes here");
+  out.close();
+  
+  
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+
 
 }
