96508d61a8b1e0dcfac37baf9492122600d286c1
Sid Ahmed Imloul <iml.sidahmed@gmail.com>
2018-02-18 22:44:49 +0100

    Delete traces of the class "data" in QuestionTextTest

--- src/test/java/io/github/oliviercailloux/collaborative_exams/helper/QuestionTextTest.java
+++ src/test/java/io/github/oliviercailloux/collaborative_exams/helper/QuestionTextTest.java
@@ -18,7 +18,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.github.oliviercailloux.collaborative_exams.helper.QuestionText;
-import io.github.oliviercailloux.collaborative_exams.model.entity.data;
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.Question;
 
 import org.junit.Assert;
