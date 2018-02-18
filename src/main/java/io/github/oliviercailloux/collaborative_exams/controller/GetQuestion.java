7cb21dfb6c4e6c1066fdacfa3b076cd0245c2b05
Sid Ahmed Imloul <iml.sidahmed@gmail.com>
2018-02-18 23:54:22 +0100

    Edit GetQuestion queryParam

--- src/main/java/io/github/oliviercailloux/collaborative_exams/controller/GetQuestion.java
+++ src/main/java/io/github/oliviercailloux/collaborative_exams/controller/GetQuestion.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
@@ -35,11 +35,11 @@ public class GetQuestion  {
        return QuestionText.QuestionsToJson(questions);
     }
 
-    @Path("/?id={id}")
+    @Path("/get")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
-    public String getQuestion(@PathParam("id") int id) throws Exception {
+    public String getQuestion(@QueryParam("id") int id) throws Exception {
         Question question = questionService.findQuestion(id);
         return QuestionText.QuestionToJson(question);
     }
