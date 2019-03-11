83b6e02e7e1a40b4a683d6043491e78ba417bb2e
Hamzaoui Mohamed <mohamed.hamzaoui@dauphine.eu>
2019-03-11 22:25:17 +0100

    add final test

--- src/test/java/io/github/oliviercailloux/collaborative_exams/helper/QuestTxtTest.java
+++ src/test/java/io/github/oliviercailloux/collaborative_exams/helper/QuestTxtTest.java
@@ -23,12 +23,13 @@ public class QuestTxtTest {
 	private String jsonQuestion ;
 	private Question q1;
 	private Answer ans;
-	private Person p1;
+	private Person p1, personne2;
 	@Before
 	@NotNull
 	public void setUp() throws Exception {
 		
 	    p1 = new Person();
+	    personne2 = new Person("mohamed.hamzaoui@dauphine.eu");
 	    ans =new Answer("True", true);
 		q1 =new Question("2 * 2 = 4 ?", "Francais", p1, QuestionType.TF, ans);	
 	}
@@ -61,16 +62,24 @@ public class QuestTxtTest {
 		  QuestionXml.unmarshallerXml(q1, "questionTest.xml");
 	    }
 	
-/*	@Test
+   @Test
 	public void testJsonToObject() throws Exception{
 		jsonQuestion  =" {\"id\":0,\"author\":{\"id\":0},\"phrasing\":\"2 * 2 = 4 ?\",\"language\":\"Francais\",\"type\":\"TF\",\"answers\":[{\"id\":0,\"text\":\"True\",\"correct\":true}],\"correct\":false}";
 		Object jsOb = QuestionText.JsonToObject(Question.class, jsonQuestion);
-		JsonReader reader = Json.createReader(new StringReader(jsonQuestion));
-		JsonObject jsonObject = reader.readObject();
-		assertEquals(jsOb,jsonObject);
+		assertNotNull(jsOb);
 		
 		
-	}*/
+	}
+
+  @Test
+  public void testObjectToJson() throws Exception{
+    String JsonObejct= QuestionText.ObjectToJson(personne2);
+     assertNotNull(JsonObejct);
+	JsonReader reader = Json.createReader(new StringReader(JsonObejct));
+	JsonObject jsonObject = reader.readObject();
+	assertTrue(jsonObject.containsKey("id"));
+	assertEquals(personne2.getEmail(),jsonObject.getString("email"));
+  }
 	}
 
 
