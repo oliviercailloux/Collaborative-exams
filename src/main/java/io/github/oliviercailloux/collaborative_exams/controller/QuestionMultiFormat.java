a1b7fbaa3a232a3485ee4e8d63683c26a8134fb6
KatiaSersour <ser-katia@hotmail.fr>
2019-03-16 16:52:16 +0100

    modifier la selection de format à renvoyer

--- src/main/java/io/github/oliviercailloux/collaborative_exams/controller/QuestionMultiFormat.java
+++ src/main/java/io/github/oliviercailloux/collaborative_exams/controller/QuestionMultiFormat.java
@@ -11,7 +11,6 @@ import javax.ws.rs.core.MediaType;
 
 import io.github.oliviercailloux.collaborative_exams.Service.QuestionService;
 import io.github.oliviercailloux.collaborative_exams.helper.QuestionText;
-import io.github.oliviercailloux.collaborative_exams.model.entity.Person;
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.Question;
 
 @Path("Multi")
@@ -22,30 +21,30 @@ public class QuestionMultiFormat {
 	
 	@Path("QuestionMultiFormat")
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	
-	public String getQuestion(@QueryParam("idQuestion") Integer idQuestion, @QueryParam("idAuthor") Integer authorId, @QueryParam("format") String format, @CookieParam("authorId") Cookie cookieIdAuthor)
-			throws Exception {
+	public String getQuestion(@QueryParam("idQuestion") Integer idQuestion, @QueryParam("idAuthor") Integer authorId, @QueryParam("format") MediaType format, @CookieParam("authorId") Cookie cookieIdAuthor)throws Exception {
 		
-		int author;
-		
+		int author;	
 		if (cookieIdAuthor == null) {
-			if (authorId == null)
+			if (authorId == null) {
 				throw new Exception("Both Cookie and the input Author Id's field are null, please log-in or register again.");
-			author = authorId;
+			} else {
+				author = authorId;
+			}
 		} else {
 			author = Integer.valueOf(cookieIdAuthor.getValue());
 		}
 		
 		Question question = questionService.findQuestion(idQuestion);
 		
-		if (format == "Json") 
+		if (format == MediaType.APPLICATION_JSON_TYPE) 
 			return QuestionText.QuestionToJson(question);
 		
-		else if (format == "XML")
-			return QuestionText.convertQuestionToXML(question);
+		else if (format== MediaType.APPLICATION_XML_TYPE)
+			return QuestionText.convertObjectToXML(question, "test.xml");
 		
 		else
-			throw new Exception("Invalide format.");
+			throw new IllegalArgumentException("Invalide format.");
 	}	
 }
