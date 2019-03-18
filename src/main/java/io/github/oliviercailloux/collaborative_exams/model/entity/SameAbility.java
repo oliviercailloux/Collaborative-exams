647ff963f4c282e66ab4a68771f79e9b71918085
ducheminvalentin <valentinduchj@gmail.com>
2019-03-18 16:02:18 +0100

    format code

--- src/main/java/io/github/oliviercailloux/collaborative_exams/model/entity/SameAbility.java
+++ src/main/java/io/github/oliviercailloux/collaborative_exams/model/entity/SameAbility.java
@@ -9,6 +9,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.Question;
+import io.github.oliviercailloux.collaborative_exams.model.entity.Person;
 
 @Entity
 @XmlRootElement(name = "RelationSameAbility")
