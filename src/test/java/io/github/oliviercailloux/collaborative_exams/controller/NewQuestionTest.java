44952159043c071866b79c0d4125bfb4f57db10e
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-27 00:05:08 +0100

    Update NewQuestionTest.java

--- src/test/java/io/github/oliviercailloux/collaborative_exams/controller/NewQuestionTest.java
+++ src/test/java/io/github/oliviercailloux/collaborative_exams/controller/NewQuestionTest.java
@@ -12,6 +12,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset; 
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
