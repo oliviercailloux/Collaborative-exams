26e679a3105c01d74d640f27ba1c6c58f6b7fddb
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-27 21:53:30 +0100

    Update ChangeQuestionLanguaugeTest.java

--- src/test/java/io/github/oliviercailloux/collaborative_exams/controller/ChangeQuestionLanguaugeTest.java
+++ src/test/java/io/github/oliviercailloux/collaborative_exams/controller/ChangeQuestionLanguaugeTest.java
@@ -27,7 +27,7 @@ public class ChangeQuestionLanguaugeTest {
 
 	@Deployment(testable = false)
 	public static WebArchive createDeployment() {
-		final File[] libs = Maven.resolver().loadPomFromFile("pom.xml")importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
+		final File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeAndTestDependencies().resolve().withTransitivity().asFile();
 		
 		final WebArchive war = ShrinkWrap.create(WebArchive.class, "examcollab.war").addPackage(ChangeQuestionLanguaugeTest.class.getPackage()).addPackages(true, "org.apache.deltaspike").addAsManifestResource(EmptyAsset.INSTANCE, "META-INF/beans.xml").addAsLibraries(libs);
 		return war;
