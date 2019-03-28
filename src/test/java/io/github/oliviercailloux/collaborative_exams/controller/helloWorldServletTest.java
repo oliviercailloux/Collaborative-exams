4e4a98c368a7032f79d3a101b6d51dc097834a54
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-28 11:09:49 +0100

    Update helloWorldServletTest.java

--- src/test/java/io/github/oliviercailloux/collaborative_exams/controller/helloWorldServletTest.java
+++ src/test/java/io/github/oliviercailloux/collaborative_exams/controller/helloWorldServletTest.java
@@ -25,8 +25,6 @@ public class helloWorldServletTest {
 	public static WebArchive createDeployment() {
 		final WebArchive war = ShrinkWrap.create(WebArchive.class, "examcollab.war")
 				.addPackage(helloWorldServletTest.class.getPackage())
-				.addPackages(true, "org.apache.deltaspike")
-				.addAsManifestResource(EmptyAsset.INSTANCE, "META-INF/beans.xml");
 		return war;
 	}
 
