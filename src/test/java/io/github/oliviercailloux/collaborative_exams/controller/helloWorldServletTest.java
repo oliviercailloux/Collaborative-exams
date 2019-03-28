efe669c78287ebe6e05edaba6d9d5a95b1a9f62f
alexdauphine <32578646+alexdauphine@users.noreply.github.com>
2019-03-28 13:04:59 +0100

    Update helloWorldServletTest.java

--- src/test/java/io/github/oliviercailloux/collaborative_exams/controller/helloWorldServletTest.java
+++ src/test/java/io/github/oliviercailloux/collaborative_exams/controller/helloWorldServletTest.java
@@ -24,7 +24,7 @@ public class helloWorldServletTest {
 	@Deployment(testable = false)
 	public static WebArchive createDeployment() {
 		final WebArchive war = ShrinkWrap.create(WebArchive.class, "examcollab.war")
-				.addPackage(helloWorldServletTest.class.getPackage())
+				.addPackage(helloWorldServletTest.class.getPackage());
 		return war;
 	}
 
