0bc1c68e530b2860a932c26ca775819b05ffb573
boumaza-khaled <37689823+boumaza-khaled@users.noreply.github.com>
2019-04-06 18:00:52 +0200

    Update TakeExam.java

--- src/main/java/io/github/oliviercailloux/collaborative_exams/controller/TakeExam.java
+++ src/main/java/io/github/oliviercailloux/collaborative_exams/controller/TakeExam.java
@@ -1,17 +1,20 @@
 package io.github.oliviercailloux.collaborative_exams.controller;
 
 import io.github.oliviercailloux.collaborative_exams.Service.AnswerQuestionService;
+import io.github.oliviercailloux.collaborative_exams.Service.AnswerService;
 import io.github.oliviercailloux.collaborative_exams.Service.ExamService;
+import io.github.oliviercailloux.collaborative_exams.Service.IQuestionService;
+import io.github.oliviercailloux.collaborative_exams.Service.PersonService;
 import io.github.oliviercailloux.collaborative_exams.model.entity.Person;
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.Answer;
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.AnswerQuestion;
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.Exam;
 import io.github.oliviercailloux.collaborative_exams.model.entity.question.IQuestion;
-import io.github.oliviercailloux.collaborative_exams.model.entity.question.QuestionType;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -21,41 +24,45 @@ import javax.ws.rs.core.MediaType;
 public class TakeExam {
 
     @Inject
+    PersonService personService;
+
+    @Inject
     ExamService examService;
+
+    @Inject
+    IQuestionService iQuestionService;
+
+    @Inject
+    AnswerService answerService;
 
     @Inject
     AnswerQuestionService answerQuestionService;
 
     /**
      *
-     * @param author
+     * @param idAuthor
      * @param idExam
-     * @param iQuestion
-     * @param answer
-     * @param freeAnswer
+     * @param idIQuestion
+     * @param idAnswer
      * @return
      * @throws Exception
      */
+    
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_PLAIN)
-    public String answerQuestion(Person author, int idExam, IQuestion iQuestion, Answer answer, String freeAnswer) throws Exception {
+    public String answerQuestion(
+            @QueryParam("idAuthor") Integer idAuthor,
+            @QueryParam("idExam ") Integer idExam,
+            @QueryParam("iQuestion ") Integer idIQuestion,
+            @QueryParam("idAnswer ") Integer idAnswer)
+            throws Exception {
+        Person author = personService.findPerson(idAuthor);
         Exam exam = examService.findExam(idExam);
-        if (iQuestion.getType() == QuestionType.Free) {
-            answerFree(exam, author, iQuestion, freeAnswer);
-        } else {
-            answerYnQcmTf(exam, author, iQuestion, answer);
-        }
-        return "OK!!";
-    }
-
-    private void answerFree(Exam exam, Person author, IQuestion iQuestion, String freeAnswer) {
-        AnswerQuestion answerQuestion = new AnswerQuestion(author, exam, iQuestion, freeAnswer);
-        answerQuestionService.persist(answerQuestion);
-    }
-
-    private void answerYnQcmTf(Exam exam, Person author, IQuestion iQuestion, Answer answer) {
+        IQuestion iQuestion = iQuestionService.findQuestion(idIQuestion);
+        Answer answer = answerService.findAnswer(idAnswer);
         AnswerQuestion answerQuestion = new AnswerQuestion(author, exam, iQuestion, answer);
         answerQuestionService.persist(answerQuestion);
+        return "OK!!";
     }
 }
